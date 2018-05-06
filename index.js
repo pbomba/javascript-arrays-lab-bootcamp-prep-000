eb79cec3ecb629f1a5294adeb80fedc68f010035
Paul Bomba` <paul@paulbomba.com>
2018-05-06 19:35:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]
+
+function destructivelyAppendKitten(array, name){
+  arrayB = array.push(name);
+  return arrayB;
+}
